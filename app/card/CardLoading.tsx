4d537009315799f99e88a69cba38db95e4588949
Syrent <56670179+Syrent@users.noreply.github.com>
2023-01-31 23:37:23 +0330

    Add pulse effect for card loading

--- app/card/CardLoading.tsx
+++ app/card/CardLoading.tsx
@@ -4,7 +4,7 @@ export default function CardLoading() {
     const cards: JSX.Element[] = [];
     for (let i = 0; i < 20; i++) {
         cards.push(
-            <div key={i} className={`flex flex-col gap-5 rounded-2xl shadow-sm dark:shadow-none bg-gray-50 dark:bg-gray-700 hover:shadow-md dark:hover:shadow-none transition-all m-2 p-6 row-span-1 col-span-4 max-h-40`}>
+            <div key={i} className={`animate-pulse flex flex-col gap-5 rounded-2xl shadow-sm dark:shadow-none bg-gray-50 dark:bg-gray-700 hover:shadow-md dark:hover:shadow-none transition-all m-2 p-6 row-span-1 col-span-4 max-h-40`}>
                 <div className="flex flex-row gap-5 items-center">
                     <FiGithub className="dark:text-white scale-[200%]" />
                     <span className="w-2/3 h-7 rounded bg-gray-300"></span>
